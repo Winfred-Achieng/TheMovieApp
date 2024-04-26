7f77cb3205762e1ffbe2a1ab8bd27932387b9513
Winfred-Achieng <achiengwinfred20@gmail.com>
2024-04-26 21:58:04 +0300

    displaying movies

--- app/src/main/java/com/winfred/themovieapp/MainActivity2.java
+++ app/src/main/java/com/winfred/themovieapp/MainActivity2.java
@@ -1,24 +1,80 @@
 package com.winfred.themovieapp;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.winfred.themovieapp.databinding.ActivityMainBinding;
+import com.winfred.themovieapp.model.Movie;
+import com.winfred.themovieapp.view.MovieAdapter;
+import com.winfred.themovieapp.viewmodel.MainActivityViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private ArrayList<Movie> movies;
+    private RecyclerView recyclerView;
+    private MovieAdapter adapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private ActivityMainBinding binding;
+    private MainActivityViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        
+        getPopularMovies();
+
+        swipeRefreshLayout=binding.swipeLayout;
+        swipeRefreshLayout.setColorSchemeResources(R.color.black);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getPopularMovies();
+            }
         });
+    }
+
+    private void getPopularMovies() {
+        viewModel.getAllMovies().observe(this, new Observer<List<Movie>>() {
+            @Override
+            public void onChanged(List<Movie> moviesFromLiveData) {
+                movies = (ArrayList<Movie>) moviesFromLiveData;
+                displayMoviesInRecyclerView();
+            }
+        });
+    }
+
+    private void displayMoviesInRecyclerView() {
+        recyclerView = binding.recyclerView;
+
+        adapter = new MovieAdapter(this,movies);
+
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+
+        adapter.notifyDataSetChanged();
+
+
     }
 }
